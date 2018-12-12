f16cd7307e9a8c3ab0f79f268391ea2781c091f2
cverbo <verbo.carlos@gmail.com>
2018-12-12 22:57:32 +0100

    Avances

--- src/test/java/com/cverbo/tfg/UserTest.java
+++ src/test/java/com/cverbo/tfg/UserTest.java
@@ -1,23 +1,25 @@
 package com.cverbo.tfg;
 
+import java.util.Arrays;
 import java.util.Date;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.cverbo.tfg.model.mongo.MongoFollowedShow;
 import com.cverbo.tfg.model.mongo.MongoUser;
+import com.cverbo.tfg.model.mongo.MongoViewedEpisode;
 import com.cverbo.tfg.service.impl.UserServiceImpl;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class UserTest {
 	
 	@Autowired
 	UserServiceImpl userServiceImpl;	
-//	static UserServiceImpl userServiceImpl;
-	
-//	@BeforeClass
-//	public static void before() {
-//		userServiceImpl = new UserServiceImpl();
-//	}
 	
 	@Test
 	public void insertUser_test() {
@@ -32,7 +34,24 @@ public class UserTest {
 		user.setPassword("1111");
 		user.setUserName("carlos.verbo");
 		
-		user.setFollowedShows(null);
+		MongoFollowedShow followedShow = new MongoFollowedShow();
+		
+		followedShow.setFavorite(false);
+		followedShow.setShowId(44217);
+		followedShow.setShowImgUrl("https://image.tmdb.org/t/p/w500/mBDlsOhNOV1MkNii81aT14EYQ4S.jpg");
+		followedShow.setShowName("Vikings");
+		
+		user.setFollowedShows(Arrays.asList(followedShow));
+		
+		MongoViewedEpisode viewedEpisode = new MongoViewedEpisode();
+		
+		viewedEpisode.setShowId(44217);
+		viewedEpisode.setSeasonId(1);
+		viewedEpisode.setEpisodeId(892432);
+		
+		
+		
+		
 		user.setFollowedUsers(null);
 		user.setViewedEpisodes(null);
 		
